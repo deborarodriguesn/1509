c3a03994c55aa47f2c56563ef827820b2657ead8
deborarodriguesn <142670397+deborarodriguesn@users.noreply.github.com>
2023-09-15 16:20:03 -0300

    Update exemplo4.R

--- exemplo4.R
+++ exemplo4.R
@@ -1,25 +1,13 @@
-#Compartilhe com a gente um código em que você usa correlação ou qui quadrado.
+# Criar um exemplo de tabela de contingência (dados categóricos)
+# Associação entre gênero e preferência por esportes
+genero <- c("Masculino", "Feminino", "Masculino", "Feminino", "Masculino")
+esporte <- c("Futebol", "Futebol", "Basquete", "Basquete", "Futebol")
 
+# Criar uma tabela de contingência
+tabela_contingencia <- table(genero, esporte)
 
-# PACOTES #
-pacman::p_load(data.table)
+# Realizar o teste qui-quadrado
+resultado_chi2 <- chisq.test(tabela_contingencia)
 
-# LEITURA PARA TESTE DE CORRELAÇÃO #
-enem_escola_pe_2019 <- fread('https://raw.githubusercontent.com/hugoavmedeiros/cp_com_r/master/bases_tratadas/ENEM_ESCOLA_2019.csv', dec = ',')
-
-cor.test(enem_escola_pe_2019$nota, enem_escola_pe_2019$TDI_03)
-# H0: variáveis são independentes / não têm associação. p-valor (p-value) > 0.05
-# H1: variáveis são dependentes / há associação. p-valor (p-value) <= 0.05
-
-t.test(enem_escola_pe_2019$nota, mu = 500.0)
-shapiro.test(enem_escola_pe_2019$nota)
-hist(enem_escola_pe_2019$nota)
-
-shapiro.test((enem_escola_pe_2019$nota - min(enem_escola_pe_2019$nota)) / (max(enem_escola_pe_2019$nota) - min(enem_escola_pe_2019$nota)))
-hist((enem_escola_pe_2019$nota - min(enem_escola_pe_2019$nota)) / (max(enem_escola_pe_2019$nota) - min(enem_escola_pe_2019$nota)))
-
-# LEITURA PARA TESTE DE FREQUÊNCIAS #
-milsa <- fread("http://www.leg.ufpr.br/~paulojus/dados/milsa.dat")
-chisq.test(milsa$civil, milsa$instrucao)
-# H0: variáveis são independentes / não há associação. p-valor (p-value) > 0.05
-# H1: variáveis são dependentes / há associação. p-valor (p-value) <= 0.05
+# Exibir o resultado
+print(resultado_chi2)
